9efec4c973ac8ce0f3431675cc394dbff257de29
misaelbazan <you@example.com>
2023-09-15 16:32:31 -0700

    solved: excess of print arg reduced at two

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
-        print('0' + str(i), end="")
-        print(", ", end="")
-    elif i == 99:
-        print('{0}'.format(i))
+    if i < 99:
+        print('%02d, ' % i, end="")
     else:
-        print(str(i), end="")
-        print(", ", end="")
+        print(i)
