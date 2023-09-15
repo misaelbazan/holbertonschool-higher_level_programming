fd11c7addb7f644fdcacad50eee063ca4146f12f
misaelbazan <you@example.com>
2023-09-14 21:00:15 -0700

    Upload: 5. 00..99 task files

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(0, 100):
+    if i < 10:
+        print('0' + str(i), end="")
+        print(", ", end="")
+    elif i == 99:
+        print(str(i))
+    else:
+        print(str(i), end="")
+        print(", ", end="")
