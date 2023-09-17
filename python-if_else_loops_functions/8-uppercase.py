f9941517be907945aa7b5316ed0162310c05a925
misaelbazan <you@example.com>
2023-09-16 21:09:58 -0700

    Solved: Indentation problem

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for i in (str):
         if ord(i) >= 97 and ord(i) <= 122:
-           convert_str = ord(i) - 32
-           i = chr(convert_str)
+            convert_str = ord(i) - 32
+            i = chr(convert_str)
         print("{0}".format(i), end="")
     print()
