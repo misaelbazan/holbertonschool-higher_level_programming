e985a70542ca517dc31d4c92be7ef29640bc0cbb
misaelbazan <you@example.com>
2023-09-16 21:06:49 -0700

    Upload: 8 task files

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in (str):
+        if ord(i) >= 97 and ord(i) <= 122:
+           convert_str = ord(i) - 32
+           i = chr(convert_str)
+        print(i, end="")
+    print()
