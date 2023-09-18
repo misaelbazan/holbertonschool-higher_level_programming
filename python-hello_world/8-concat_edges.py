8c656354ecd31d906c0f9fd10f563e4c4c3e6747
misaelbazan <you@example.com>
2023-09-17 21:06:46 -0700

    Solved: str concatenating instead joining

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\ language that combines remarkable power with very clear syntax"
-str = str.split()[5:7]
-str = " ".join(str) + ' with Python'
+str = "Python is an interpreted, interactive, object-oriented programming\
+        language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[113:119] + str[:6]
 print(str)
