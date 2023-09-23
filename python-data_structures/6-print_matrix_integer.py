9b8a1f0b57952e57b453281ea987503e9cfe5779
misaelbazan <you@example.com>
2023-09-22 20:38:06 -0700

    Upload: 6th task files. Correct: spaces at the end of rows

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for val in row:
-            print("{:d}".format(val), end=" ")
+        for val in row[:-1]:
+            print("{:d} ".format(val), end="")
+        for val in row[-1:]:
+            print("{:d}".format(val), end="")
         print()
