293a1c45386988ccbb41c79cc7fb708fc104eb0d
misaelbazan <you@example.com>
2023-09-28 20:26:17 -0700

    Git addA

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    return(len(sentence), None if sentence == "" else sentence[0])
+    if sentence == "":
+        return (len(sentence), None)
+    else:
+        return (len(sentence), sentence[0])
