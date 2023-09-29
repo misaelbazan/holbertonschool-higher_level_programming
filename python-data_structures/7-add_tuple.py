1953905400dc30aca6584515a23111326f3b9c55
misaelbazan <you@example.com>
2023-09-28 19:47:14 -0700

    AAAAs

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,7 +3,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_a += (0, 0)
     tuple_b += (0, 0)
 
-    addition_0 = tuple_a[0] + tuple_b[0]
-    addition_1 = tuple_a[1] + tuple_b[1]
+    addition_0 = int(tuple_a[0]) + int(tuple_b[0])
+    addition_1 = int(tuple_a[1]) + int(tuple_b[1])
     addition = int(addition_0), int(addition_1)
     return(addition)
