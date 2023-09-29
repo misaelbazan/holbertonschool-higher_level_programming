0444d23d5727129698827a411a6bb6d5f8272ce9
misaelbazan <you@example.com>
2023-09-28 20:17:43 -0700

    Changed: added a tuple variable

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,8 @@
 def multiple_returns(sentence):
     first = sentence[0]
     length = len(sentence)
+    tuple_a = length, first
     if len(sentence) <= 0:
-        return(length, None)
+        tuple_a = length, None
     else:
-        return(length, first)
+        return(tuple_a)
