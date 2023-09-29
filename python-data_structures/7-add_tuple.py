6aa6eff84d65258326f781abe581ca88ac73d41f
misaelbazan <you@example.com>
2023-09-28 19:36:53 -0700

    Upload: 7th task files

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+
+    addition_0 = tuple_a[0] + tuple_b[0]
+    addition_1 = tuple_a[1] + tuple_b[1]
+    addition = addition_0, addition_1
+    return(addition)
