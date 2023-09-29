61d0282c6c9a53e29f3a676de7178e0290f5a044
misaelbazan <you@example.com>
2023-09-28 20:14:32 -0700

    Change: if condition

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     first = sentence[0]
     length = len(sentence)
-    if not sentence:
+    if len(sentence) <= 0:
         return(length, None)
     else:
         return(length, first)
