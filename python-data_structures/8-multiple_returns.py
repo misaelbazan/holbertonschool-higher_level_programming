cd414baba3bc1f188db2151bbc83f1a7cd560414
misaelbazan <you@example.com>
2023-09-28 20:20:14 -0700

    Changed: first value on function file

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@ def multiple_returns(sentence):
     first = sentence[0]
     length = len(sentence)
     tuple_a = length, first
-    if len(sentence) <= 0:
-        tuple_a = length, None
+    if not sentence:
+        first = None
     else:
         return(tuple_a)
