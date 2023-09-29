42687f9e97ea4d023a778bedd3af142845fb8d85
misaelbazan <you@example.com>
2023-09-28 20:07:11 -0700

    Upload: 8th task files(1)

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if not sentence:
-        first = None
+        sentence[0] = None
     else:
         length = len(sentence)
         first = sentence[0]
