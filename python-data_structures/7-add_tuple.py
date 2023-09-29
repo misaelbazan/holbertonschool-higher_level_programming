448f0130776cac7baadfdc82276f7795789896de
misaelbazan <you@example.com>
2023-09-28 19:51:02 -0700

    return changed

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,8 +2,4 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     tuple_a += (0, 0)
     tuple_b += (0, 0)
-
-    addition_0 = int(tuple_a[0]) + int(tuple_b[0])
-    addition_1 = int(tuple_a[1]) + int(tuple_b[1])
-    addition = int(addition_0), int(addition_1)
-    return(int(addition))
+    return(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
