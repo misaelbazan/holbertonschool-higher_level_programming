cc92aa1157760f78a9dfd436d056ff1cfb1c0e54
misaelbazan <you@example.com>
2023-09-28 20:23:08 -0700

    Changed: Return condition

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    first = sentence[0]
-    length = len(sentence)
-    tuple_a = length, first
-    if not sentence:
-        first = None
-    else:
-        return(tuple_a)
+    return(len(sentence), None if sentence == "" else sentence[0])
