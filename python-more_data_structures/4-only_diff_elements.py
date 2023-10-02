009be83baf4c576337a974a616ad5d7c19544402
misaelbazan <you@example.com>
2023-10-02 16:43:58 -0700

    Upload: 4th task files

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    od_set =
+    set_union = set_1.union(set_2)
+    set_intersection = set_1.intersection(set_2)
+    diff_list = {x for x in set_union if x not in set_intersection}
+    return(list(diff_list))
