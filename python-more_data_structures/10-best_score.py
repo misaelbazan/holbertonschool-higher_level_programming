0687144684174e96b76fa8fb8d847f541553ac9c
misaelbazan <you@example.com>
2023-10-03 19:52:29 -0700

    upload: 10th task files(2)

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,6 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return(None)
+
     else:
         high_score = max(a_dictionary, key=lambda x: a_dictionary[x])
         return(high_score)
