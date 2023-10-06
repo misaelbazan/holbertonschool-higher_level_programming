250f39e94cab9137540b4afaa63d9d99563b4fd8
misaelbazan <you@example.com>
2023-10-05 20:20:46 -0700

    Correct: number of elements printed addressed in a variable

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
+        print_count = 0
         for item in range(x):
             print(my_list[item], end="")
+            print_count += 1
         print()
-        return(x)
+        return(print_count)
     except IndexError:
         print()
-        return(x)
+        return(print_count)
