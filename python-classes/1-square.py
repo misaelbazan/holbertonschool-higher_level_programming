83ed6bde68ac0878e7e3e147f606bc0c6a0e26ce
misaelbazan <you@example.com>
2023-10-08 20:44:12 -0700

    Upload: 1st task files

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Class Square Definition:
+"""
+
+
+class Square:
+    """
+    Square Class Definition
+    """
+
+    def __init__(self, size):
+        """
+        Here the Square class is initialized
+
+        Args:
+            size (int o float): Size measure of square
+        """
+        self.__size = size
