c64ceca3bff0ca92bf6cae8bae980c7785b32b2b
misaelbazan <you@example.com>
2023-10-11 18:30:50 -0700

    AADadsaf

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, size=0):
+        """Initialize an Object of the Square class
+        Args:
+            self: 
+            size: 0 by default
+        """
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
