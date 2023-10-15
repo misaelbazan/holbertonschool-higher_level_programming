07fd580db328f20b1885def01fec932aee709030
misaelbazan <misaelbfranco@gmail.com>
2023-10-14 20:06:51 -0700

    File: 1-rectangle, documentations added to each method

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -11,13 +11,20 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.__height = height
         self.__width = width
+        """Initialize a new instance of Rectangle
+        Args:
+            width: the the width of the rectangle
+            height: the height of the rectangle
+        """
 
     @property
     def width(self):
+        """Returns the actual width of the Obj"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Sets the width value from the outside"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -26,10 +33,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """Returns the height value"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Sets the height value from the outside"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
