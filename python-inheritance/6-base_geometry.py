93843de1345598503ff2a92d16d5694d0287a340
misaelbazan <misaelbfranco@gmail.com>
2023-10-17 19:43:45 -0700

    File: 6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains: 01 classes"""
+
+
+class BaseGeometry:
+    """Base Geometry is in construction"""
+    def area(self):
+        """This method is raises an exception"""
+        raise Exception("area() is not implemented")
