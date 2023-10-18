fb157cf2c2268494c2e68635fc64c27bf72fa82d
misaelbazan <misaelbfranco@gmail.com>
2023-10-17 19:04:33 -0700

    Changed: doctest added to is_same_class method

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -7,6 +7,11 @@
 
 
 def is_same_class(obj, a_class):
+    """This method evaluates if an object is an instance of a specified class
+    Return:
+        True: if obj is exactly an instance of the specified class
+        False: otherwise
+    """
     if type(obj) == a_class:
         return True
     else:
