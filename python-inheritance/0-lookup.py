cc29fb169d8763fd08f594adc7b1cbebda276a66
misaelbazan <misaelbfranco@gmail.com>
2023-10-17 17:11:04 -0700

    File: 0-lookup

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module contains:
+"""
+
+
+def lookup(obj):
+    """This function returns a list
+    Return:
+        list: of available attributes and method of an object
+    """
+    return(dir(obj))
