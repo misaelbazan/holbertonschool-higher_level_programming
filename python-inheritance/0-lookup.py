c135a001795721e86593e89e011c050026dee8b2
misaelbazan <misaelbfranco@gmail.com>
2023-10-17 17:14:57 -0700

    README file was forgotten

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -6,6 +6,8 @@ This module contains:
 
 def lookup(obj):
     """This function returns a list
+    Args:
+        obj: object with attributes to return
     Return:
         list: of available attributes and method of an object
     """
