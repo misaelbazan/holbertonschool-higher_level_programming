76d122f99ba80ba93dc517d16ca707f2e4c236a8
misaelbazan <misaelbfranco@gmail.com>
2023-12-08 17:50:34 -0800

    10-top_score.sql

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- lists all records of the table 'second_table
+-- displaying only score and name rows
+-- records should be ordered in descending form
+SELECT score, name FROM second_table
+ORDER BY score DESC;
