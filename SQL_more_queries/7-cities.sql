88de94bf740af751ea71267dd834c8893856444f
misaelbazan <misaelbfranco@gmail.com>
2023-12-12 21:34:42 -0500

    7-cities.sql file

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,14 @@
+-- create the database 'hbtn_0d_usa' and the table 'cities' inside it
+-- cities description:
+--  id INT unique, auto-generated, can't be NULL and PK
+--  state_id INT, can't be NULL and must be a FK that references to 'id'
+--  name VARCHAR(256) can't be NULL
+-- if 'hbtn_0d_usa' already exists, the script should not fail
+-- id 'cities' already exists, the scriot should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  state_id INT NOT NULL,
+  name VARCHAR(256) NOT NULL
+);
