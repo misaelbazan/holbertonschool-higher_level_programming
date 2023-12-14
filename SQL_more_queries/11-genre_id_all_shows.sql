81cd3a08ee8066ff83c458bb14b434ae251ac89c
misaelbazan <misaelbfranco@gmail.com>
2023-12-13 19:29:18 -0800

    11-genre_idd file

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,6 +3,7 @@
 -- results must be sorted by ASC order by tv_shows.title
 -- and tv_show_genres.genre_id
 -- if a show doesn't have a genre, display NULL
+
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
