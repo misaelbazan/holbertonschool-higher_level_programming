2b55222b6cf5df79d79d690d6ffa072fdf39e35c
misaelbazan <misaelbfranco@gmail.com>
2023-12-14 11:08:00 -0800

    0-select_states file 3

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -16,19 +16,14 @@ The code should not be executed when imported
 import sys
 import MySQLdb
 
-
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-
 # Establish a connection
 if __name__ == "__main__":
     db = MySQLdb.connect(
             host="localhost",
             port=3306,
-            user=username,
-            passwd=password,
-            db=database,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
             charset="utf8"
         )
     cur = db.cursor()
