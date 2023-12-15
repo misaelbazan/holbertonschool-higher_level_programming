1a005ed82e20c5008a9d67f7fe55aba81cced9cc
misaelbazan <misaelbfranco@gmail.com>
2023-12-15 11:03:46 -0800

    model_state.py file

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Creating a class that inherits from Base"""
+
+    __tablename__ = 'states'
+
+    """Declaring class attributes"""
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
