e4f656f016f24ac18e02a4b7d5c59be7d724b13a
misaelbazan <misaelbfranco@gmail.com>
2023-12-26 18:03:59 -0800

    2 task file 4

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    """Prints a list of anything, but only prints the integers
+    Return:
+        Number of integers printed
+    """
     nb_integers = 0
     for i in range(x):
         try:
