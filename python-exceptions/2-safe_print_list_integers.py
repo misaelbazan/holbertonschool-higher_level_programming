805dd908a9d890ad333594574e71322ab85c9443
misaelbazan <misaelbfranco@gmail.com>
2023-12-26 17:59:02 -0800

    2 task file 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,6 +8,6 @@ def safe_print_list_integers(my_list=[], x=0):
             nb_integers += 1
         except (ValueError, TypeError):
             continue
-    
+ 
     print()
     return(nb_integers)
