c7de90a6a16e85e259f24b7ac2b4291ccdede8a0
misaelbazan <misaelbfranco@gmail.com>
2023-12-26 18:00:35 -0800

    2 task file 3

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     nb_integers = 0
-
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
             nb_integers += 1
         except (ValueError, TypeError):
             continue
- 
     print()
     return(nb_integers)
