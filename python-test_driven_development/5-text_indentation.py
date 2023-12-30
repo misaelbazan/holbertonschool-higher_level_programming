9a96d1bca392e0c5da2081191d20edc032b93fbc
misaelbazan <misaelbfranco@gmail.com>
2023-12-29 19:30:22 -0800

    5th task files

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -11,11 +11,11 @@ def text_indentation(text):
     result = ""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    for char in text:
-        result += char
-        if char in chars:
-            result += "\n\n"
-    bresult = [line.lstrip() for line in result.split('\n')]
-    formated_txt = '\n'.join(bresult)
-
-    print(formated_txt, end="")
+    if type(text) == str:
+        for char in text:
+            result += char
+            if char in chars:
+                result += "\n\n"
+        bresult = [line.lstrip() for line in result.split('\n')]
+        formated_txt = '\n'.join(bresult)
+        print(formated_txt, end="")
